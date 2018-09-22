358857b59ffccfd39dc0d1be345e1e3d424d1a75
Nishant Rayan <nrayan@lyft.com>
2018-09-22 16:42:59 -0700

    refactor to allow shuttle time

--- web/slider.js
+++ web/slider.js
@@ -1,11 +1,19 @@
-$(document).ready(function(){
+function getNextShuttleTime(){
+    return new Date($.now());
+}
+function fmtTime(dt){
+    return dt.getHours() + ":" + dt.getMinutes();
+}
+function fmtDate(dt){
     const monthNames = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"
     ];
-    var dt = new Date($.now());
-    var currentTime = dt.getHours() + ":" + dt.getMinutes();
-    var currentDate = monthNames[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getYear();
-    $('.currentDate').text(currentDate)
-    $('.currentTime').text(currentTime);
+    return monthNames[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getYear();
+}
+$(document).ready(function(){
 
+    var dt = new Date($.now());
+    $('.currentDate').text(fmtDate(dt))
+    $('.currentTime').text(fmtTime(dt));
+    $('.nextShuttleTime').text(fmtTime(getNextShuttleTime()));
 });
