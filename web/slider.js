3ee1be4cc2fc04d45cbf30321116807ecb9f37a9
Nishant Rayan <nrayan@lyft.com>
2018-09-22 16:30:20 -0700

    show current time

--- web/slider.js
+++ web/slider.js
@@ -1,1 +1,6 @@
-$('.currentTime').text('Current time is')
+$(document).ready(function(){
+    var currentTime = new Date($.now());
+    var formattedTime = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+
+    $('.currentTime').text('Current time is '+ formattedTime);
+});
