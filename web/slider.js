b6b199e7f2feaf006faa4e79246ad33c2cec9a69
Nishant Rayan <nrayan@lyft.com>
2018-09-22 16:38:28 -0700

    add current date

--- web/slider.js
+++ web/slider.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-    var currentTime = new Date($.now());
-    var formattedTime = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    var dt = new Date($.now());
+    var currentTime = dt.getHours() + ":" + dt.getMinutes();
+    var currentDate = monthNames[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getYear();
+    $('.currentDate').text(currentDate)
+    $('.currentTime').text(currentTime);
 
-    $('.currentTime').text('Current time is '+ formattedTime);
 });
