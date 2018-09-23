c2bcf586d0c3b645ff64160b958fdca4ef462b5f
Nishant Rayan <nrayan@lyft.com>
2018-09-23 10:59:58 -0700

    restart only if there is update

--- scripts/run_shuttle.sh
+++ scripts/run_shuttle.sh
@@ -1,4 +1,34 @@
+ check_update()
+ {
+    UPSTREAM=${1:-'@{u}'}
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse "$UPSTREAM")
+    BASE=$(git merge-base @ "$UPSTREAM")
+
+    if [ $LOCAL = $REMOTE ]; then
+        echo "uptodate"
+    elif [ $LOCAL = $BASE ]; then
+        echo "pull"
+    elif [ $REMOTE = $BASE ]; then
+        echo "push"
+    else
+        echo "diverged"
+    fi
+ }
+ ./scripts/show_shuttle.sh
  while true; do
-    /home/pi/src/slider/scripts/show_shuttle.sh
-    cd /home/pi/src/slider && git pull --rebase
+    pushd /home/pi/src/slider
+    git remote update
+    update=$(check_update)
+    if [ "$update" == "pull" ]; then
+        echo "Updates in remote"
+        git stash
+        git pull --rebase
+        git stash pop
+        ./scripts/show_shuttle.sh
+    else
+        echo "No updates in remote"
+    fi
+    sleep 2
+    popd
 done
--- scripts/show_shuttle.sh
+++ scripts/show_shuttle.sh
@@ -1,3 +1,2 @@
-epiphany-browser  ~/src/slider/web/slider.html &
-sleep 10;
 pgrep -fl epiphany-browser | cut -d" " -f1 | xargs kill -9;
+epiphany-browser  ~/src/slider/web/slider.html &
