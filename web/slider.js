bd74bae7f43c51da2279749e7f5efb59b83ce722
Nishant Rayan <nrayan@lyft.com>
2018-09-22 17:08:04 -0700

    shuttle time

--- web/slider.js
+++ web/slider.js
@@ -1,5 +1,23 @@
 function getNextShuttleTime(){
-    return new Date($.now());
+    const shuttleTimeStrings = ['18:00']
+    const shuttleTimes = $.map(shuttleTimeStrings, function(ts){
+        var hm = ts.split(":");
+        var hour = parseInt(hm[0]);
+        var minute = parseInt(hm[1]);
+        var dt = new Date($.now());
+        dt.setHours(hour);
+        dt.setMinutes(minute);
+        return dt;
+    });
+    var nextShuttleTimes = $.grep(shuttleTimes, function(dt){
+        console.log(fmtDate(dt));
+        return dt.getTime() >= new Date($.now()).getTime();
+    });
+    if(nextShuttleTimes.length == 0){
+        return null;
+    } else {
+        return nextShuttleTimes[0];
+    }
 }
 function fmtTime(dt){
     return dt.getHours() + ":" + dt.getMinutes();
